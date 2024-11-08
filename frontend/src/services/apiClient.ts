7c4a93250cf6e683ea3d4df8a4fbfc911c817e43
0xHippo <37877455+alliu930410@users.noreply.github.com>
2024-11-07 19:48:43 -0700

    Add apiClient

---
+++ frontend/src/services/apiClient.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+import { useDynamicToken } from "@/context/DynamicTokenContext";
+
+const apiClient = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+});
+
+export const useAuthenticatedApiClient = () => {
+  const { token } = useDynamicToken();
+  apiClient.interceptors.request.use((config) => {
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  });
+
+  return apiClient;
+};
