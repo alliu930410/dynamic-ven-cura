da9396a9e8aa367b2242306bc40c3a97cee0b2ef
0xHippo <37877455+alliu930410@users.noreply.github.com>
2024-11-08 08:33:11 -0700

    Add createWallet service function

--- backend/src/custodial/custodial.service.ts
+++ backend/src/custodial/custodial.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { pick } from 'lodash';
 import { GetCustodialWalletsDto } from './custodial.dto';
+import { generateKey } from 'src/utils/keygen';
+import { encryptKey } from 'src/utils/crypto';
 
 @Injectable()
 export class CustodialService {
@@ -22,5 +24,62 @@ export class CustodialService {
     return wallets.map((wallet) =>
       pick(wallet, ['address', 'nickName', 'publicKey', 'createdAt']),
     );
+  }
+
+  async createWallet(dynamicUserId: string): Promise<GetCustodialWalletsDto> {
+    // Generate a new key pair
+    const { address, publicKey, privateKey } = generateKey();
+
+    // Encode the private key for storage
+    const { encryptedKey, iv } = encryptKey(privateKey);
+
+    // Parse default nickName
+    const totalWallets = await this.prismaService.custodialWallet.count({
+      where: {
+        user: {
+          dynamicUserId,
+        },
+      },
+    });
+
+    // Note: this might have conditions if multiple wallets are created at the same time
+    // we can fix this by using a transaction or raw SQL query
+    // but for the sake of simplicity, we will ignore this for now
+    const nickName = `Account ${totalWallets + 1}`;
+
+    // Create user if not already exists, then create & associate wallet
+    await this.prismaService.user.upsert({
+      where: { dynamicUserId },
+      update: {
+        custodialWallets: {
+          create: {
+            address,
+            nickName,
+            privateKey: encryptedKey,
+            privateKeyVI: iv,
+            publicKey,
+          },
+        },
+      },
+      create: {
+        dynamicUserId,
+        custodialWallets: {
+          create: {
+            address,
+            nickName,
+            privateKey: encryptedKey,
+            privateKeyVI: iv,
+            publicKey,
+          },
+        },
+      },
+    });
+
+    return {
+      address,
+      nickName,
+      publicKey,
+      createdAt: new Date(),
+    };
   }
 }
