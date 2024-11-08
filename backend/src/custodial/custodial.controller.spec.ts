e0126a3ae0900d613d3095a6597739c2c28e210b
0xHippo <37877455+alliu930410@users.noreply.github.com>
2024-11-08 08:40:33 -0700

    Split different controllers into describe blocks

--- backend/src/custodial/custodial.controller.spec.ts
+++ backend/src/custodial/custodial.controller.spec.ts
@@ -33,71 +33,72 @@ describe('CustodialController', () => {
     await prismaService.user.deleteMany();
   });
 
-  it('should return 401 Unauthorized error if accessToken is missing', async () => {
-    const res = await request(app.getHttpServer())
-      .get(`/custodial/wallets`)
-      .expect(401);
+  describe('GET /custodial/wallets', () => {
+    it('should return 401 Unauthorized error if accessToken is missing', async () => {
+      const res = await request(app.getHttpServer())
+        .get(`/custodial/wallets`)
+        .expect(401);
 
-    expect(res.body).toMatchInlineSnapshot(`
+      expect(res.body).toMatchInlineSnapshot(`
 {
   "message": "Unauthorized",
   "statusCode": 401,
 }
 `);
-  });
-
-  it('should return empty array if user has no custodial wallets', async () => {
-    // Prep: generate a token that resolves to a user with no custodial wallets
-    const token = generateJwtToken({
-      sub: 'user-with-no-custodial-wallets',
     });
 
-    const res = await request(app.getHttpServer())
-      .get(`/custodial/wallets`)
-      .set('Authorization', `Bearer ${token}`)
-      .expect(200);
+    it('should return empty array if user has no custodial wallets', async () => {
+      // Prep: generate a token that resolves to a user with no custodial wallets
+      const token = generateJwtToken({
+        sub: 'user-with-no-custodial-wallets',
+      });
 
-    expect(res.body).toMatchInlineSnapshot(`[]`);
-  });
+      const res = await request(app.getHttpServer())
+        .get(`/custodial/wallets`)
+        .set('Authorization', `Bearer ${token}`)
+        .expect(200);
 
-  it('should return list of wallets if user has custodial wallets by ascending creation order', async () => {
-    // Prep: generate a token that resolves to a user with no custodial wallets
-    const token = generateJwtToken({
-      sub: 'user-with-custodial-wallets',
+      expect(res.body).toMatchInlineSnapshot(`[]`);
     });
 
-    // Prep: create user with 2 custodial wallets
-    await prismaService.user.create({
-      data: {
-        dynamicUserId: 'user-with-custodial-wallets',
-        custodialWallets: {
-          create: [
-            {
-              address: '0x123',
-              nickName: 'Account 1',
-              privateKey: 'test_private_key_1',
-              privateKeyVI: 'test_string_iv_1',
-              publicKey: 'test_public_key_1',
-              createdAt: new Date('2024-11-08T00:00:00.000Z'),
-            },
-            {
-              address: '0x456',
-              privateKey: 'test_private_key_2',
-              privateKeyVI: 'test_string_iv_2',
-              publicKey: 'test_public_key_2',
-              createdAt: new Date('2024-11-07T00:00:00.000Z'),
-            },
-          ],
+    it('should return list of wallets if user has custodial wallets by ascending creation order', async () => {
+      // Prep: generate a token that resolves to a user with no custodial wallets
+      const token = generateJwtToken({
+        sub: 'user-with-custodial-wallets',
+      });
+
+      // Prep: create user with 2 custodial wallets
+      await prismaService.user.create({
+        data: {
+          dynamicUserId: 'user-with-custodial-wallets',
+          custodialWallets: {
+            create: [
+              {
+                address: '0x123',
+                nickName: 'Account 1',
+                privateKey: 'test_private_key_1',
+                privateKeyVI: 'test_string_iv_1',
+                publicKey: 'test_public_key_1',
+                createdAt: new Date('2024-11-08T00:00:00.000Z'),
+              },
+              {
+                address: '0x456',
+                privateKey: 'test_private_key_2',
+                privateKeyVI: 'test_string_iv_2',
+                publicKey: 'test_public_key_2',
+                createdAt: new Date('2024-11-07T00:00:00.000Z'),
+              },
+            ],
+          },
         },
-      },
-    });
+      });
 
-    const res = await request(app.getHttpServer())
-      .get(`/custodial/wallets`)
-      .set('Authorization', `Bearer ${token}`)
-      .expect(200);
+      const res = await request(app.getHttpServer())
+        .get(`/custodial/wallets`)
+        .set('Authorization', `Bearer ${token}`)
+        .expect(200);
 
-    expect(res.body).toMatchInlineSnapshot(`
+      expect(res.body).toMatchInlineSnapshot(`
 [
   {
     "address": "0x456",
@@ -113,5 +114,8 @@ describe('CustodialController', () => {
   },
 ]
 `);
+    });
   });
+
+  describe('POST /custodial/wallets', () => {});
 });
