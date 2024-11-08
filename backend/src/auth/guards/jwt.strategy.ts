ec6606ff2374922928350e5323b77ed71a5360b2
0xHippo <37877455+alliu930410@users.noreply.github.com>
2024-11-07 21:50:03 -0700

    Add JwtAuthGuard

---
+++ backend/src/auth/guards/jwt.strategy.ts
@@ -0,0 +1,33 @@
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable } from '@nestjs/common';
+import { Request } from 'express';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromExtractors([JwtStrategy.extractJWT]),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_PUBLIC_KEY,
+      algorithms: ['ES256'],
+    });
+  }
+
+  private static extractJWT(req: Request): string | null {
+    if (
+      req.cookies &&
+      'accessToken' in req.cookies &&
+      req.cookies.accessToken.length > 0
+    ) {
+      return req.cookies.accessToken;
+    }
+
+    return null;
+  }
+
+  async validate(payload: any, done: any) {
+    const { iat, exp, sub, sid, iss, ...rest } = payload;
+    return done(null, rest);
+  }
+}
