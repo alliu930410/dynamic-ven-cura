952900a06516b51fcded092b24cdf6d1ce3db453
0xHippo <37877455+alliu930410@users.noreply.github.com>
2024-11-08 11:27:40 -0700

    Add swagge OpenAPI setups

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +9,14 @@ async function bootstrap() {
   app.enableCors({
     origin: 'http://localhost:3003',
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('VenCura API')
+    .setDescription('Custodial Wallet API')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   await app.listen(3000);
 }
