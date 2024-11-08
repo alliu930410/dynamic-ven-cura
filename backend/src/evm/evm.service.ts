14a0a69fdb50fc8cf9dcfa2de9f2a2d264118a85
0xHippo <37877455+alliu930410@users.noreply.github.com>
2024-11-08 10:10:13 -0700

    Switch support to Sepolia & Base Sepolia

--- backend/src/evm/evm.service.ts
+++ backend/src/evm/evm.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { Alchemy, Network } from 'alchemy-sdk';
-import { sepolia, polygonAmoy } from 'viem/chains';
+import { sepolia, polygonAmoy, baseSepolia } from 'viem/chains';
 import { InvalidChainIdException } from './evm.exceptions';
 import { ethers } from 'ethers';
 
 export enum SUPPORTED_CHAIN_IDS {
   SEPOLIA = sepolia.id,
-  POLYGON_AMOY = polygonAmoy.id,
+  BASE_SEPOLIA = baseSepolia.id,
 }
 
 @Injectable()
@@ -19,9 +19,9 @@ export class EVMService {
         apiKey: process.env.ALCHEMY_API_KEY,
         network: Network.ETH_SEPOLIA,
       }),
-      [polygonAmoy.id]: new Alchemy({
+      [baseSepolia.id]: new Alchemy({
         apiKey: process.env.ALCHEMY_API_KEY_BE,
-        network: Network.MATIC_AMOY,
+        network: Network.BASE_SEPOLIA,
       }),
     };
   }
