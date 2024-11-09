0e4520a85b0db36d7720bf2207d87679224c8977
0xHippo <37877455+alliu930410@users.noreply.github.com>
2024-11-08 22:28:42 -0700

    Add messsage history

--- frontend/src/components/WalletMessageHistory.tsx
+++ frontend/src/components/WalletMessageHistory.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useAuthenticatedApiClient } from "@/services/apiClient";
 
 interface CustodialWallet {
   address: string;
@@ -13,20 +14,49 @@ interface WalletMessageHistoryProps {
 const WalletMessageHistory: React.FC<WalletMessageHistoryProps> = ({
   selectedWallet,
 }) => {
+  const apiClient = useAuthenticatedApiClient();
   const [messageHistory, setMessageHistory] = useState<any[]>([]);
 
+  useEffect(() => {
+    const fetchMessageHistory = async () => {
+      try {
+        const response = await apiClient.get(
+          `/custodial/wallet/messages/${selectedWallet.address}`
+        );
+        setMessageHistory(response.data.items);
+      } catch (error: any) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchMessageHistory();
+  }, []);
+
   return (
-    <div className="w-full h-full">
+    <div className="w-full h-full bg-gray-100 flex items-center justify-center">
       {messageHistory.length > 0 ? (
-        <ul>
-          {messageHistory.map((message, index) => (
-            <li key={index}>
-              <p>Message: {message}</p>
-            </li>
-          ))}
-        </ul>
+        <div className="w-full max-w-md bg-white shadow-lg rounded-lg overflow-hidden">
+          <h2 className="text-lg font-semibold text-gray-800 border-b p-4">
+            Message History
+          </h2>
+          <ul className="divide-y divide-gray-200">
+            {messageHistory.map((message, index) => (
+              <li key={index} className="p-4">
+                <p className="text-sm text-gray-600">
+                  <span className="font-bold">Message:</span> {message.message}
+                </p>
+                <p className="text-sm text-gray-500">
+                  <span className="font-bold">Signed:</span>{" "}
+                  {new Date(message.createdAt).toLocaleString()}
+                </p>
+              </li>
+            ))}
+          </ul>
+        </div>
       ) : (
-        <p>No message history available.</p>
+        <p className="text-gray-500 text-center">
+          No message history available.
+        </p>
       )}
     </div>
   );
